2c216e3b2dd300331e5fa84bf99e4b805856e528
Xa-C24 <9335@holbertonstudents.com>
2024-06-17 14:54:57 +0200

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 
 {
-puts("Programming is like building a multilingual puzzle, \n");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
