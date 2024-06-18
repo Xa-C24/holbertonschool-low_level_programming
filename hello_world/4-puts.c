8818e11608e1e4c903f84d9f8ee7b125344e937d
Xa-C24 <9335@holbertonstudents.com>
2024-06-18 12:10:22 +0200

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main  program that prints a line with puts function
+ * main - prints "program that prints a line with puts function"
  * puts: print the message
- * return: value 0 
- */
+ * return: value 0
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
