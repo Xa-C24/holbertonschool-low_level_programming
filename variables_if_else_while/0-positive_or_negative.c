ffec127c92ef29fb2ef215d8bb08e0688ee83c3f
Xa-C24 <9335@holbertonstudents.com>
2024-06-18 16:37:03 +0200

    task0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,16 +5,16 @@
 {
 	int n;
 
-	srand(time) (0);
+	srand(time (0));
 	n = rand () - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
@@ -22,4 +22,5 @@
 	}
 	return (0);
 }
+
 
