92883f4b03626fc0d37dc2801cebe3dc8d94ea19
Xa-C24 <9335@holbertonstudents.com>
2024-06-18 12:26:21 +0200

    task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main: A program that prints a line with print function
- * printf: print the message
- * return: value 0 (Sucess) 
- */
+ * main  program that prints a line with print function
+ *
+ * printf print the message
+ * return value 0 (Sucess)
+*/
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
