639f57e6ae174a03de08713a1541970627b51317
Xa-C24 <9335@holbertonstudents.com>
2024-06-18 10:34:25 +0200

    task4

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - A program that prints a line with puts function
+ * printf: print the message
+ * Return: value 0 (Sucess) 
+ */
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
