6c60c412e05caf1fa02db4a525ee6a1411ea2919
Xa-C24 <9335@holbertonstudents.com>
2024-06-18 11:18:52 +0200

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main: A program that prints a line with puts function
  * puts: print the message
  * Return: value 0 (Sucess) 
  */
