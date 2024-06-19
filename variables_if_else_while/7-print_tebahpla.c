94c458b5701b2ef9b4aea91f33eb62514efe6b02
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 14:01:40 +0200

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+/**
+ * main - Prints the last digit of a randomly generated number
+ * Descrption:Prints the numbers 1 à 9
+ *
+ * Return: Always 0.
+ */
+{
+	char low;
+	
+	for (low = 'z' ; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+return (0);
+}
