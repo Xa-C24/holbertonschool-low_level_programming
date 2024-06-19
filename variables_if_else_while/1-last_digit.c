63b67dadfa14743fc0157834b5d8aef539e8799a
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 10:20:02 +0200

    task1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,32 +2,37 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
- * Description: program of description
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
  *
- * Return: 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    int n, lastd;
-
+    int n;
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-
-if (lastd > 5)
-{
-printf("Last digit of %d and is greater than 5\n", n, lastd);
-}
-else if (lastd == 0)
-{
-printf("last digit of %d and is 0\n", n, lastd);
-}
-else if (lastd < 6 && lastd ! =0)
-{
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-}
+    if ((n % 10) > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n",
+            n, n % 10);
+    }
+    else if ((n % 10) < 6 && (n % 10) != 0)
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n",
+            n, n % 10);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is 0\n",
+            n, n % 10);
+    }
     return (0);
 }
+
+
+
+
 
 
 
