9eb0865ea9efbf301f34a596ef10a499bc6d02ee
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 10:45:55 +0200

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+*main - Prints the alphabetic
+*Return: value 0 always
+*/
+int main(void)
+{
+	char az;
+	for (az = 'a' ; az <= 'z' ; az++)
+	putchar(az);
+	putchar('\n');
+	return (0);
+}
+
+
