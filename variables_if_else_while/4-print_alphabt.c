71851f021da5d5303e2fd74086dc557859507702
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 12:09:00 +0200

    task4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char low, e, q;
+
+    e = 'e';
+    q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+    {
+	if (low != e && low != q)
+    putchar(low);
+    }
+	putchar('\n');
+	return (0);
+}
