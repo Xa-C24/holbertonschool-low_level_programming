a94ae9ed3178df8d281860d4f8ea6ad5abd9f0f8
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 15:31:44 +0200

    task9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 	int a;
-	for (a = '0' ; a <= '9'; a++)
+	for (a = 0 ; a <= 9; a++)
 	{
-		putchar(a);
-		if (a != '9')
+		putchar(a + '0');
+		if (a != 9)
 	{
 		putchar(',');
+		putchar(' ');
 	}
 	}
 	putchar('\n');
