57d955248f0d5441a9fcc6e91afe6a76bb54165f
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 21:16:16 +0200

    task5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,18 +6,11 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int a, j;
-
-	for (a = 0; a < 10; a++)
-
-	for (j = 0; j <= 10; j++)
-
-	if (j > 9)
-	putchar((j / 10) + '0');
-	putchar((j % 10) + '0');
+	char a;
+	for (a = '1' ; a <= '9' ; a++)
+	putchar(a + '0');
 	putchar('\n');
 	return (0);
 }
