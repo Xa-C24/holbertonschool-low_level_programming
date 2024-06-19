d9fca22251ebfa6004d27a16a3d3ac4f27eabed0
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 15:45:21 +0200

    task3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Prints the last digit of a randomly generated number
  *        and whether it is greater than 5, less than 6, or 0.
