86573587a8512c2a3c747130c97bff60de5c343a
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 15:54:10 +0200

    task3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,11 @@
 int main(void)
 {
 char c;
-
-for (c = 'a'; c <= 'z'; c++);
+for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 }
-for (c = 'A' ; c <= 'Z'; c++);
+for (c = 'A' ; c <= 'Z'; c++)
 {
 putchar(c);
 }
