ec21684b8e80f03a91bf79b6febe95e464ed660b
Xa-C24 <9335@holbertonstudents.com>
2024-06-19 12:11:37 +0200

    task4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	char low, e, q;
 
-    e = 'e';
-    q = 'q';
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-    {
+{
 	if (low != e && low != q)
-    putchar(low);
-    }
+	putchar(low);
+}
 	putchar('\n');
 	return (0);
 }
