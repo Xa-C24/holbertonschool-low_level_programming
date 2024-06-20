5039c492a2c0a8e828c358159940d4f353973985
Xa-C24 <9335@holbertonstudents.com>
2024-06-20 16:47:07 +0200

    task3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int o = 0;
+	int n = 1;
+
+	while (n <= 1)
+	{
+	for (o = '0' ; o <= '1' ; n++)
+	{
+	putchar(o);
+	}
+	putchar('\n');
+	n++;
+	}
+	return (0);
+}
