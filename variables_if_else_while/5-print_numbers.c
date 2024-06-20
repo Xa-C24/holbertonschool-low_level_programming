7b8f8020adefae13f5cf88d9fe4b51bcb3c85ba8
Xa-C24 <9335@holbertonstudents.com>
2024-06-20 09:48:09 +0200

    task5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
+int main(void)
 /**
- * main - Prints the numbers
+ * int main - Prints the last digit of a randomly generated number
+ * Descrption:Prints the numbers 1 à 9
  *
  * Return: Always 0.
  */
-int main(void)
 {
-	char a;
-	for (a = 1 ; a <= 9 ; a++)
+	int a = '0';
+
+	while (a <= '9')
 	{
-	putchar(a + '0');
+		putchar(a);
+		a++;
 	}
-	{
 	putchar('\n');
-	}
-	return (0);
+return (0);
 }
