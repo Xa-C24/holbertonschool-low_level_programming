a72abbaada124af275476bd8388005fe6aac3bf7
Xa-C24 <9335@holbertonstudents.com>
2024-06-20 15:09:46 +0200

    task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,12 +8,10 @@ int main(void)
 {
 		char c[8] = "_putchar";
 		int a;
-		for (a = 0; a <= 8; a++)
-		{
-			_putchar(c[a]);
-		}
-		_putchar('\n');
-		
-		return (0);
+	for (a = 0; a <= 8; a++)
+{
+	_putchar(c[a]);
 }
-	
+	_putchar('\n');
+	return (0);
+}
