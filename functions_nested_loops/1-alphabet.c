99e1ee325c73eb8803407e938f9a2da75c27d9ac
Xa-C24 <9335@holbertonstudents.com>
2024-06-20 15:36:10 +0200

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: value always 0
+*/
+int main(void)
+{
+	char az;
+	for (az = 'a' ; az <= 'z' ; az++)
+	{
+	putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
