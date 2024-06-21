4ec6897141a5e7ec6c91a6c666f7e22e0b638f2a
Xa-C24 <9335@holbertonstudents.com>
2024-06-21 19:50:17 +0200

    task11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,18 @@
 /**
  * 
  */
-void print_to_98(int n);
-
+void print_to_98(int n)
 {
 	if (n >= 98)
 	{
 		while (n > 98)
-		printf("%d, ", n--);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-		printf("%d, ", n++);
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 }
