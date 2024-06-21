dd11c189842d1b81f2471cfa1212544b88466b31
Xa-C24 <9335@holbertonstudents.com>
2024-06-21 20:04:15 +0200

    task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -11,6 +11,13 @@
  * Return: value always 0
  */
 int main(void)
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: value always 0
+ */
+int main(void)
 {
 	char c[8] = "_putchar";
 	int a;
