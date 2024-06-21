abc1ce0984f4f5d366ab148afefefbe2a84a0eef
Xa-C24 <9335@holbertonstudents.com>
2024-06-21 10:17:25 +0200

    task4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * -isalpha: check alphabetic character
- *c: character checked
+ * isalpha: check alphabetic character
+ * 
+ * Description c: character checked
  * Return: 1 for character, Always 0.
  */
 int _isalpha(int c)
