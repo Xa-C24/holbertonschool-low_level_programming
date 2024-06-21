cdf019abdb963c4a58cc15b3d0a47d4ae3212bb3
Xa-C24 <9335@holbertonstudents.com>
2024-06-21 13:34:58 +0200

    task6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,17 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
- * 
+ * main - check the code
+ *
+ * Return: value always a
  */
 int _abs(int a)
 {
-	if (_abs < 0)
-	_putchar(97)
-	{
-	return(-0);
-	}
-	else (a > 0)
-	
-	return(0);
+	if (a < 0)
+	a= -a;
+	return(a);
 }
