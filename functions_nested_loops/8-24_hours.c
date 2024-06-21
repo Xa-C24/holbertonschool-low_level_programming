1d8067e239ce4c0e284de53fb3ecdaac62c104b4
Xa-C24 <9335@holbertonstudents.com>
2024-06-21 15:28:40 +0200

    task8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,11 +6,9 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	h = 0;
-	while (h < 24)
+	for (h = 0; h <= 23; h++)
 	{
-		m = 0;
-		while (m < 60)
+	for (m = 0; m <= 59; m++)
 	 	{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
@@ -18,7 +16,6 @@ void jack_bauer(void)
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
-			m++;
 		}
-	}   h++;
+	}   
 }
