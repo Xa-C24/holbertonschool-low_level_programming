ed2b7d5b1fe4cede926259e3eee90e97272a35db
Xa-C24 <9335@holbertonstudents.com>
2024-06-24 10:36:14 +0200

    task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
- * 
+ * _isupper - detect upper case alphabets
+ * @c: input character
+ *
+ * Return: (1) for right else (0) for fasle
+ *
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'a')
 	{
 	return (1);
 	}
 	else
+	{
 	return (0);
 	}
+}
