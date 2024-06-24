9a094e4d8efee7accb0b5945447817e70b694145
Xa-C24 <9335@holbertonstudents.com>
2024-06-24 13:33:20 +0200

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *more_numbers: Print 10 times the numbers 0 to 14.
+ *
+ *Return: 10 times of numbers 0 to 14.
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+		for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+	{
+		if (b > 9)
+		_putchar (b / 10 + '0');
+		_putchar (b % 10 + '0');
+	}
+		_putchar('\n');
+}
+}
