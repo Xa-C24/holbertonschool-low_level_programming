59ae6454604872c5ef62a97c28bf4fe0c5a3f395
Xa-C24 <9335@holbertonstudents.com>
2024-06-24 10:51:22 +0200

    task1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit: Check if a caracter it's a digit
+ * @c: number checked
+ *
+ *Return : 1 if truc, 0 if fasle
+ */
+int _isdigit(int c)
+{
+   if (c >= '0' && c <= "9")
+   {
+	return (1);
+   }
+	else 
+	return (0);
+}
