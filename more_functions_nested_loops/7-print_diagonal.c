3c387ac84d966e3f0ba0460f39924ebad47f74cb
Xa-C24 <9335@holbertonstudents.com>
2024-06-24 15:28:37 +0200

    tas7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
+ * Description: Can only use _putchar to print
+ */
+
+void print_diagonal(int n)
+{
+	int l;
+	int d;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 0; l <= n; l++)
+		{
+		for (d = 0; d < l; d++)
+		{
+			_putchar(' ');
+		}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
