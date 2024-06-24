b255e0698ff7d48bc147253d983a549efabdfc2c
Xa-C24 <9335@holbertonstudents.com>
2024-06-24 15:54:57 +0200

    task3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * Print number: print number 0 to 9
- * 
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ *
  */
+
 void print_numbers(void)
 {
 	int a;
