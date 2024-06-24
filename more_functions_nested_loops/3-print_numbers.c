b22e2b69d6ebf4ce730fe9ae8b0d857cbd999853
Xa-C24 <9335@holbertonstudents.com>
2024-06-24 12:12:10 +0200

    task3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * print number 0 to 9
  * 
@@ -8,7 +7,9 @@ void print_numbers(void)
 {
 	int a;
 
-	for (a >= '0' ; '9'; a++)
-	_putchar(a);
-	_putchar('\n');
+	for (a = 0 ; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+		_putchar('\n');
 }
