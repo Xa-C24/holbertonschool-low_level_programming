ccc8a5f122913018bddfcd0afd24eb1641fedda5
Xa-C24 <9335@holbertonstudents.com>
2024-06-25 11:55:12 +0200

    1main.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int: funtion with two type pointers
+ * @a: first arguement
+ * @b: second argument 
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int temporaire;
+
+		temporaire = *a;
+		*a = *b;
+		*b = temporaire;
+}
