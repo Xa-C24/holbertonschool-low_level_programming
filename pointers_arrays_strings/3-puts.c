d4399e18c609d3d9707cf33ffc4dd3b91db53a70
Xa-C24 <9335@holbertonstudents.com>
2024-06-25 14:16:57 +0200

    task3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts - print the argument
+ * @str: one char type argument
+ *
+ * Description: print string
+ *Return: return (0)
+ */
+void _puts(char *str)
+{
+	char str;
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	_puts ("str");
+
+	return (0);
+}
