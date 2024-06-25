d912823f1e64712aebc32c72ae0b1254e519dae7
Xa-C24 <9335@holbertonstudents.com>
2024-06-25 15:12:36 +0200

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (*(s + a) != '\0')
+	{
+		a++;
+	}
+		a -= 1;
+		while (a > 0)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
