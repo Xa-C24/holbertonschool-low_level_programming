b584d23dc45f61836f6e6cf3d6ab1d45dcba91df
Xa-C24 <9335@holbertonstudents.com>
2024-06-26 10:52:01 +0200

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int b, ip;
+
+	b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	ip = b;
+	if (ip % 2 != 0)
+		ip = (ip +1) /2;
+
+		else
+			ip /= 2;
+		while (ip < b)
+		{
+			ip++;
+			_putchar (*(str + ip -1));
+		}
+		_putchar('\n');
+}
