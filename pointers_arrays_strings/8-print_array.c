9146919d4618ef6640ddb197781b6db58d689769
Xa-C24 <9335@holbertonstudents.com>
2024-06-26 12:55:15 +0200

    task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * 
+ * 
+ */
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf ("%d", *(a + i));
+		if (i < n - 1)
+		printf (", ");
+	}
+		_putchar('\n');
+}
