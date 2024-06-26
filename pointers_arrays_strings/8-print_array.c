42ef0f1b0a5fd59770eba59118d3a9674a5b45ba
Xa-C24 <9335@holbertonstudents.com>
2024-06-26 13:20:41 +0200

    task8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
- * 
+ * print_array - function with two argument
+ * @a: first argument pointer int type
+ * @n: second argument int type
+ *
+ * Description: prints n element of an array of integers
+ * Return: na
  */
 void print_array(int *a, int n)
 {
@@ -15,5 +19,5 @@ void print_array(int *a, int n)
 		if (i < n - 1)
 		printf(", ");
 	}
-		printf('\n');
+		_putchar('\n');
 }
