5d794f9888805ad797025a4703de4dfa4eb41081
Xa-C24 <9335@holbertonstudents.com>
2024-06-28 13:58:01 +0200

    task1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,22 @@
 #include "main.h"
 /**
- * 
+ *
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
 
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
 }
