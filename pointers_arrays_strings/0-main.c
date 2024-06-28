c92b39ce04717c2afb2c18dc724acfcee31c050f
Xa-C24 <9335@holbertonstudents.com>
2024-06-28 16:27:43 +0200

    task_2

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-     char s1[98] = "Hello ";
+    char s1[98] = "Hello ";
     char s2[] = "World!\n";
     char *ptr;
 
--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,5 +10,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
-		return (0);
+
+	return (dest);
 }
