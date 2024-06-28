50c73795364116403ac41e0d1bdf10e49b08b309
Xa-C24 <9335@holbertonstudents.com>
2024-06-28 13:07:39 +0200

    main.h

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,8 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 
-#include "main.h"
-#include <stdio.h>
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
 
 /**
  * main - check the code
