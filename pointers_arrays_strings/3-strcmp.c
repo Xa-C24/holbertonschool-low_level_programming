a33ad228c504ec7146694aefb97b25c42944564e
Xa-C24 <9335@holbertonstudents.com>
2024-06-30 11:51:14 +0200

    task3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *
  *
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-
-	 printf("%d\n", _strcmp(s1, s2));
-
 	if (*s1 == *s2)
 	{
 		return (0);
