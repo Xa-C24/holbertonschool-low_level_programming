d6a7a5ec1f46bd452874549ebfbba125ef1c1d75
Xa-C24 <9335@holbertonstudents.com>
2024-07-01 12:20:32 +0200

    task1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - Fills n bytes of memory white char c
+ * @s: pointer to first byte of memory
+ * @b: character to fill memory
+ * @n: number of bytes fo fill
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
