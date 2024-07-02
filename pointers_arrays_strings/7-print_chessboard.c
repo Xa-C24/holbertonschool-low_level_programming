17ee0a15f788a32e5cca0bfc2ea137f132facace
Xa-C24 <9335@holbertonstudents.com>
2024-07-02 10:48:16 +0200

    task6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+
+		_putchar(a[x][y]);
+	}
+
+		_putchar('\n');
+}
