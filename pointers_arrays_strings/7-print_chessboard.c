fb80fd2787620c19fca61556518adf0cbc7dc83d
Xa-C24 <9335@holbertonstudents.com>
2024-07-02 13:06:57 +0200

    task7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,11 @@ void print_chessboard(char (*a)[8])
 	int x, y;
 
 	for (x = 0; x < 8; x++)
-	
-		for (y = 0; y < 8; y++)
-	{
 
-		_putchar(a[x][y]);
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+
 		_putchar('\n');
 	}
 }
