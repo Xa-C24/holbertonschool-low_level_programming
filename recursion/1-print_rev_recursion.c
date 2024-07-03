794bb85091a47e054b41a2949c31cacb0aa2c582
Xa-C24 <9335@holbertonstudents.com>
2024-07-03 09:47:45 +0200

    task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- * 
+ *-print_rev_recursion - print a string
+ * @s: pointer
  * 
  */
 void _print_rev_recursion(char *s)
 {
-	
-
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
