e0bddfddc9af07749d92fa72adc8c7a7113bd816
Xa-C24 <9335@holbertonstudents.com>
2024-07-03 20:09:33 +0200

    main.h

--- recursion/main.h
+++ recursion/main.h
@@ -1,12 +1,11 @@
 #include <unistd.h>
 #ifndef main_h
 #define main_h
-#include <math.h>
 
-int check_square(int a, int b)
 
 int _putchar(char c);
 int check_prime(int divisor, int n);
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
