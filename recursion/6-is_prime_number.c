5c83b885fc4f6848d116158dba76e508c022bb94
Xa-C24 <9335@holbertonstudents.com>
2024-07-03 19:06:48 +0200

    task6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,37 +1,46 @@
-#include "main.h"
+	#include "main.h"
+	#include <stdio.h>
+	#include <math.h>
 
-/**
- *
- *
- */
-int is_prime_helper(int n, int i)
-{
-    if (i > sqrt(n))
-    {
-        return (1);
-    }
-    if (n % i == 0)
-    {
-        return (0);
-    }
-    return (is_prime_helper(n, i + 1));
-}
+	/* Déclaration de la fonction récursive */
+	int check_prime(int divisor, int n);
 
-/**
- * is_prime_number - vérifie si un nombre est premier
- * @n: le nombre à vérifier
- *
- * Return: 1 si le nombre est premier, sinon 0
- */
-int is_prime_number(int n)
-{
-    if (n <= 1)
-    {
-        return (0);
-    }
-    if (n == 2)
-    {
-        return (1);
-    }
-    return (is_prime_helper(n, 2));
-}
+	/**
+	 * is_prime_number - vérifie si un nombre est premier
+	 * @n: le nombre à vérifier
+	 *
+	 * Return: 1 si le nombre est premier, sinon 0
+	 */
+	int is_prime_number(int n)
+	{
+		if (n <= 1)
+		{
+			return (0);
+		}
+		if (n == 2)
+		{
+			return (1);
+		}
+		return (check_prime(2, n));
+	}
+
+	/**
+	 * check_prime - aide à vérifier si un nombre est premier de manière récursive
+	 * @divisor: le diviseur actuel
+	 * @n: le nombre à vérifier
+	 *
+	 * Return: 1 si le nombre est premier, sinon 0
+	 */
+	int check_prime(int divisor, int n)
+	{
+		if (divisor > sqrt(n))
+		{
+			return (1);
+		}
+		if (n % divisor == 0)
+		{
+			return (0);
+		}
+		return (check_prime(divisor + 1, n));
+	}
+	
