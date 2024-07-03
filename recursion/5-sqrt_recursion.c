221fabfb41483614c48aa2c7e882a1edaffce307
Xa-C24 <9335@holbertonstudents.com>
2024-07-03 14:48:49 +0200

    task5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,15 +3,20 @@
  *
  *
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int i, int n)
 {
-	if (n < 0)
-		return (0);
+	return (check_square(0, n));
+}
 
-	for (int i = 0; i * i <= n; i++)
-	{
-		if (i * i == n)
-			return (1);
-	}
-	return (0);
+int check_square(int i, int n)
+{
+int sq = i * i;
+
+	if (sq > n)
+		return (-1);
+
+	if (sq == n)
+		return (i);
+
+	return (check_square(i + 1, n));
 }
