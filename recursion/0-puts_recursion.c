256bfe2c5293594416e03b6065ae60514761bb11
Xa-C24 <9335@holbertonstudents.com>
2024-07-03 09:28:12 +0200

    task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_puts_recursion - use _putchar for print string
+ * @s: pointer
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
