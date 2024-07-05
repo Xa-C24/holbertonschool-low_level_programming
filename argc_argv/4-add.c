5173d64843a4214dad7bc920e07aca15ddb1fc76
Xa-C24 <9335@holbertonstudents.com>
2024-07-05 15:40:44 +0200

    task4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
  *
  *
  *
  */
+
 int main (int argc, char *argv[])
 
 {
-
-	int a, b, result;
-
-	if (argc <= 2)
+	int sum = 0;
+	if (argv == 1)
 	{
-		printf("Error\n");
-			return (1);
+		printf ("0\n");
+		return (0);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a + b;
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+			sum += atoi(argv[i]);
+	}
 
-		printf("%d\n", result);
+	printf("%d\n", sum);
 
-			return (0);
+	return (0);
 }
