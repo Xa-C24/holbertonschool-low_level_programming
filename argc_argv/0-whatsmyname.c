014791e1afc7c5f839f194ca61dbbcb92941c0cd
Xa-C24 <9335@holbertonstudents.com>
2024-07-05 15:42:28 +0200

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/**
+
+ /**
+ * main - Prints name of command called
+ * @argc: number of arguments including command name
+ * @argv: Pointer to array of pointers to arguments as strings including
+ *			command name
  *
- *
- *
- *
+ * Return: 0 for success, 1 for error
  */
 int main(int argc, char **argv)
 {
