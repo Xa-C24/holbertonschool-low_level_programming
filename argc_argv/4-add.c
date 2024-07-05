2158dac1809009b197c56f7a0323262c25bbd711
Xa-C24 <9335@holbertonstudents.com>
2024-07-05 15:04:34 +0200

    task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main (int argc, char *argv[])
+
+{
+
+	int a, b, result;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+			return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a + b;
+
+		printf("%d\n", result);
+
+			return (0);
+}
