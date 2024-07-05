d4a63eb53e3433d318db5614936f4698af2a4e13
Xa-C24 <9335@holbertonstudents.com>
2024-07-05 14:50:05 +0200

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- *
- *
- */
-int main (int argc, char *argv[])
+	#include <stdio.h>
+	#include <stdlib.h>
+	/**
+	 *
+	 *
+	 */
+	int main (int argc, char *argv[])
+
 {
 	int a, b, result;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+		if (argc <= 2)
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
 
-	printf("%d", result);
+	printf("%d\n", result);
 
-	return (0);
+		return (0);
 }
