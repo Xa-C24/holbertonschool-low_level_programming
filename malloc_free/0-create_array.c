b73c0a96165e5bdd7ff65796e38df99a2f551af6
Xa-C24 <9335@holbertonstudents.com>
2024-07-08 12:15:54 +0200

    task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * create_array - Entrer tab
+ * @size: size of tab
+ * @c : character in the tab
+ *
+ *Return: pointer to tab, NULL if fails
+*/
+
+*create_array(unsigned int size, char c)
+{
+	char *array;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = (char *)malloc(size * sizeof(char));
+		if (array == NULL)
+	{
+		return (NULL);
+	}
+
+		for (i = 0; i < size; i++)
+
+		{
+		array[i] = c;
+		}
+		return (array);
+}
