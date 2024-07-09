5dd3e80813bca6998a79d6ce53395e42620572fe
Xa-C24 <9335@holbertonstudents.com>
2024-07-09 14:56:03 +0200

    task2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,8 +8,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *concat;
 	unsigned int len = n, index;
-	char *string_nconcat;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -18,27 +18,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (index = 0; s1[index]; index++)
-	{
 		len++;
-	}
-	string_nconcat = malloc(sizeof(char) * (len + 1));
 
-		if (string_nconcat == NULL)
+	concat = malloc(sizeof(char) * (len + 1));
 
-		len = 0;
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
 
 	for (index = 0; s1[index]; index++)
-	{
-		string_nconcat[len++] = s1[index];
-	}
+		concat[len++] = s1[index];
 
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
-	for (index = 0; s2[index]; index++)
-	{
-		string_nconcat[len++] = s2[index];
-	}
+	concat[len] = '\0';
 
-	string_nconcat[len] = '\0';
-
-		return (string_nconcat);
+	return (concat);
 }
