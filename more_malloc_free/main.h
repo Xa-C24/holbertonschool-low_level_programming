3d61896bdca41be09dd20626316f23ecb5bc5dfc
Xa-C24 <9335@holbertonstudents.com>
2024-07-09 10:02:52 +0200

    main.h

---
+++ more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
