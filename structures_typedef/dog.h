824f45d05169f300edc68068586de699d972dc69
Xa-C24 <9335@holbertonstudents.com>
2024-07-12 10:22:29 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,14 +6,6 @@ struct  dog
 	char *name;
 	float *age;
 	char *owner;
-}
-dog_t;
-
-int _putchar(char c);
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
 
 #endif
