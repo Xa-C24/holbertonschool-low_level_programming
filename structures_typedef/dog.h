843f355a36f6995a2588638a9dddd5d5dee83e1a
Xa-C24 <9335@holbertonstudents.com>
2024-07-12 10:15:58 +0200

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+struct  dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+dog_t;
+
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
