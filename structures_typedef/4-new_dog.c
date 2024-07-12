fdbf621b602c24306e5966ca309034ebd6e9a3f5
Xa-C24 <9335@holbertonstudents.com>
2024-07-12 16:04:39 +0200

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,13 +9,50 @@
  * @owner: owner of the dog
  *
  */
+	int _strlen(char *s)
+{
+		int i;
+		i = 0;
+
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+		return (i);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len ++;
+	}
+
+	for (i = 0; i < len; i++)
+		{
+			dest[i] = src[i];
+		}
+			dest[i] = '\0';
+		
+		return (dest);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	char *name_copy, *owner_copy;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL)
+	{
 		return (NULL);
+	}
+
+	if	(owner == NULL)
+	{
+		return (NULL);
+	}
+
 
 	dog = malloc(sizeof(dog_t));
 		if (dog == NULL)
@@ -29,7 +66,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog);
 			return (NULL);
 		}
-	strcpy(name_copy, name);
+	_strcpy(name_copy, name);
 
 	owner_copy = malloc(strlen(owner) + 1);
 		if (owner_copy == NULL)
@@ -38,7 +75,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog);
 			return (NULL);
 		}
-	strcpy(owner_copy, owner);
+	_strcpy(owner_copy, owner);
 
 	dog->name = name_copy;
 	dog->age = age;
