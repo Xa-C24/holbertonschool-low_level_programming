055e17baebf956b7d91472bc48a91cd695d045ad
Xa-C24 <9335@holbertonstudents.com>
2024-07-15 11:26:05 +0200

    task0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
+* print_name - prints name Bob.
+ * @name: input Bob.
+ *@f: function pointer.
  *
- *
- *
+ * Return: no retour.
  */
 
 void print_name(char *name, void (*f)(char *))
