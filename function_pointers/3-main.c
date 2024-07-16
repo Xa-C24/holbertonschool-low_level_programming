c87e9a26d47fe037e98d48895b7ce01ba9d6914c
Xa-C24 <9335@holbertonstudents.com>
2024-07-16 17:06:07 +0200

    3-main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * main - check the code HSs
+ *@argc: argument count
+ *@argv: argument vector
  *
- *
- *
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*operation)(int, int);
+	char operator;
 
 	if (argc != 4)
 	{
@@ -21,17 +24,22 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 
 	operation = get_op_func(argv[2]);
-
 	if (operation == NULL)
 		{
 			printf("Error\n");
 				exit(99);
 		}
 
+		operator = *argv[2];
+		if ((operator == '/' || operator == '%') && num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
 	result = operation(num1, num2);
 	{
 		printf("%d\n", result);
 	}
-
 		return (0);
 }
