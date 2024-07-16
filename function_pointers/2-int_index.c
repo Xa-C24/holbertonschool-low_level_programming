2505e9c2244d284b227d8e856bac06a733d252f2
Xa-C24 <9335@holbertonstudents.com>
2024-07-16 16:42:22 +0200

    task2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,9 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 		int index;
 
-	if (array && cmp)
 	{
-		if (size <= 0)
+		if (size <= 0 || array == NULL || *cmp == NULL)
 			return (-1);
 	}
 
