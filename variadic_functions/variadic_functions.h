3999367cd98e96359063260994af111b958ed653
Xa-C24 <9335@holbertonstudents.com>
2024-07-18 11:25:21 +0200

    variadic_functions.h

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef _VARIADIC_FONCTION_H
+#define _VARIADIC_FONCTION_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
