95f8a12d08093e9c907f872100e68c3b245ef9c9
Xa-C24 <9335@holbertonstudents.com>
2024-08-01 15:10:01 +0200

    Correction code sans strlen: Write a function that adds a new node at the beginning of a list_t list.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,35 +1,46 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
- /**
- * add_node - add a new node at beginning of the list.
- * @head: Pointer to the adress.
- * @str: Pointer to linked list.
- * Return: Adress new element.
- */
+	/**
+	 * add_node - add a new node at beginning of the list.
+	 * @head: Pointer to the adress.
+	 * @str: Pointer to linked list.
+	 * Return: Adress new element.
+	 */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *tmp;
+	list_t *new_node;
+	char *dup_str;
+	unsigned int len = 0;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	if (head == NULL || str == NULL)
+	{
+		return (NULL);
+	}
 
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
-	tmp = strdup(str);
-	new_node->str = tmp;
 
-	if (new_node->str == NULL)
+	dup_str = strdup(str);
+	if (dup_str == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(tmp);
-	new_node->next = *head;
+	/* Calculate length of string manually */
+	while (str[len])
+	len++;
 
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
-}
+	}
