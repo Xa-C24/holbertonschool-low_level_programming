ba124a04a8d10494dc97809379d1921f21f24660
Xa-C24 <9335@holbertonstudents.com>
2024-08-01 12:00:31 +0200

    corection: Write a function that adds a new node at the beginning of a list_t list.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,6 +8,8 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *tmp;
+
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
@@ -15,14 +17,15 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->str = strdup(str);
+	tmp = strdup(str);
+	new_node->str = tmp;
 
 	if (new_node->str == NULL)
 	{
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = strlen(tmp);
 	new_node->next = *head;
 
 	*head = new_node;
