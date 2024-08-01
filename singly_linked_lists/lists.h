d0caa8e630d2ebeb4377da22e95a19f2405e7518
Xa-C24 <9335@holbertonstudents.com>
2024-08-01 10:31:52 +0200

    modification: list.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
 typedef struct list_s
 {
 	char *str;
