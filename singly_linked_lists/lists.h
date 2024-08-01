2ebd85428f09ae917439bf6ec87751c2606198b8
Xa-C24 <9335@holbertonstudents.com>
2024-08-01 10:19:22 +0200

    list.h

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+int _putchar(char c);
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
