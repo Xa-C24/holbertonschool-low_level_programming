e164e842c55ce26b00c1c6b2c7b1182e14111ffa
Xa-C24 <9335@holbertonstudents.com>
2024-08-02 10:25:56 +0200

    Correction code: add value of len. Create a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,21 +10,21 @@
  * Return: 1 on sucess, -1 for error.
  *
  */
-int create_file(const char *filename, char *text_content)	/* name of file; content of file. */
+int create_file(const char *filename, char *text_content)
+/* name of file; content of file. */
 {
-	int file_d, st_wr, len; 				/* file desciptor, stock write, len of text.*/
+	int file_d, st_wr, len = 0;
+	/* file desciptor, stock write, len of text.*/
 
 		if (filename == NULL)
-		{
 			return (-1);
-		}
+
 
 	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 		if (file_d == -1)
-		{
 			return (-1);
-		}
+
 
 	if (text_content != NULL)
 	{
