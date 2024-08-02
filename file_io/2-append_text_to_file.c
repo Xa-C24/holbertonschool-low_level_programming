4c4fdd0a8a087e53e7a74ea286fa98674a81dded
Xa-C24 <9335@holbertonstudents.com>
2024-08-02 12:19:43 +0200

    Correction Code + Betty style,add if.Write a function that appends text at the end of a file.

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,17 +13,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	size_t len = 0;
 
 	if (filename == NULL)
-			return (-1);
+		return (-1);
 
-	file = open (filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
-			return (-1);
+		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
-	}
+
 
 	bytes_written = write(file, text_content, len);
 		{
@@ -31,6 +31,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			close(file);
 			return (-1);
 		}
+	}
 
 		close(file);
 		return (1);
